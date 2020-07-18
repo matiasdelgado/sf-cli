4a6de7106873260b9ffcca2945f28fb8f147b6f2
Matias Delgado <matiasdel@gmail.com>
2020-07-17 22:51:03 -0300

    Add flag to show only debug messages in log command

--- src/commands/log.js
+++ src/commands/log.js
@@ -1,12 +1,17 @@
-const { Command } = require('@oclif/command');
+const { Command, flags } = require('@oclif/command');
 const showLogs = require('../helpers/scratch-log');
 
 class LogCommand extends Command {
   async run() {
-    showLogs();
+    const { flags } = this.parse(LogCommand);
+    showLogs(flags.debug);
   }
 }
 
 LogCommand.description = 'Show remote logs';
+
+LogCommand.flags = {
+  debug: flags.boolean({ char: 'd', description: 'Show debug messages only' })
+};
 
 module.exports = LogCommand;
--- src/helpers/scratch-log.js
+++ src/helpers/scratch-log.js
@@ -2,7 +2,11 @@ const spawn = require('child_process').spawn;
 
 module.exports = showLogs;
 
-function showLogs() {
-  const sfdxProcess = spawn('sfdx', ['force:apex:log:tail', '--color'], { stdio: ['ignore', 'pipe', 'ignore'] });
-  spawn('grep', ['USER_DEBUG'], { stdio: [sfdxProcess.stdio[1], process.stdout, process.stdout] });
+function showLogs(debug) {
+  if (debug) {
+    const sfdxProcess = spawn('sfdx', ['force:apex:log:tail', '--color'], { stdio: ['ignore', 'pipe', 'ignore'] });
+    spawn('grep', ['USER_DEBUG'], { stdio: [sfdxProcess.stdio[1], process.stdout, process.stdout] });
+  } else {
+    spawn('sfdx', ['force:apex:log:tail', '--color'], { stdio: 'inherit' });
+  }
 }
